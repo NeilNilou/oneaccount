464065159e13d8191e8d20a1562e633667875663
Neil Nilou <neildu91@hotmail.fr>
2017-11-13 13:09:53 +0100

    feat(routes): added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Homepage from '@/components/Homepage';
+import Setup from '@/components/Setup';
+import About from '@/components/About';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Homepage',
-      component: Homepage,
-    },
+    { path: '/', name: 'Homepage', component: Homepage },
+    { path: '/setup', name: 'Setup', component: Setup },
+    { path: '/about', name: 'About', component: About },
   ],
+  mode: 'history',
 });
