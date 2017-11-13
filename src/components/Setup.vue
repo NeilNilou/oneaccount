0f302ff35c2e171e38b443dcb0d40faaa9ca8b12
Neil Nilou <neildu91@hotmail.fr>
2017-11-13 14:19:16 +0100

    Refactor(Setup): setup page with form to creation a new IDCard

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -9,7 +9,56 @@
         <router-link to='/about'><md-button class="router-link-btn">about us</md-button></router-link>
       </div>
     </md-toolbar>
-    <h1>New setup</h1>
+
+    <md-card class="card-setup">
+      <md-card-header>
+        <div class="md-title">Create a new identity</div>
+      </md-card-header>
+
+      <md-card-content class="card-content">
+        <form novalidate @submit.stop.prevent="submit">
+          <md-input-container class="md-input-invalid" md-clearable>
+            <md-icon>account_box</md-icon>
+            <label>First name</label>
+              <md-input placeholder="Enter your first name" required></md-input>
+              <span class="md-error">The name you entered is not valid</span>
+          </md-input-container>
+
+          <md-input-container class="md-input-invalid" md-clearable>
+            <md-icon>account_box</md-icon>
+            <label>Last name</label>
+              <md-input placeholder="Enter your last name" required></md-input>
+              <span class="md-error">The name you entered is not valid</span>
+          </md-input-container>
+
+          <md-input-container class="md-input-invalid">
+          <md-icon>face</md-icon>
+          <label>Avatar</label>
+          <md-file v-model="onlyImages" accept="image/*" placeholder="Upload a picture to create an avatar"></md-file>
+          <span class="md-error">The image you uploaded is not valid</span>
+          </md-input-container>
+
+          <md-input-container class="md-input-invalid" md-clearable>
+            <md-icon>phone</md-icon>
+            <label>Phone</label>
+              <md-input placeholder="Enter your phone number" required></md-input>
+              <span class="md-error">The phone number you entered is not valid</span>
+          </md-input-container>
+
+          <md-input-container class="md-input-invalid" md-clearable>
+            <md-icon>email</md-icon>
+            <label>Email</label>
+              <md-input placeholder="Enter your email address" required></md-input>
+              <span class="md-error">The email address you entered is not valid</span>
+          </md-input-container>
+        </form>
+        <md-button href="#" class="md-button md-raised md-primary" id="new-id-btn">
+          Next
+          <md-icon>send</md-icon>
+        </md-button>
+      </md-card-content>
+    </md-card>
+
   </main>
 </template>
 
@@ -39,5 +88,14 @@ export default {
 .router-link-btn {
   color: white;
   text-decoration: none;
+}
+.card-setup {
+  margin: 0 auto;
+  height: auto;
+  width:30%;
+  margin-top: 5%;
+}
+#new-id-btn {
+  margin-top: 5%;
 }
 </style>
