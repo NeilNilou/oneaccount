8e3fda1a447364b45e85cdee6e865a3c150cb3a7
Neil Nilou <neildu91@hotmail.fr>
2017-11-15 12:31:40 +0100

    fix(Homepage): renamed Homepage to Home

---
+++ src/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <main>
+    <md-toolbar class="md-medium">
+    <router-link to='/'><img class="logo-navbar" src="../assets/logo-nav.png"></router-link>
+
+      <div class="navbar-btn">
+        <router-link to='/'><md-button class="router-link-btn">access data</md-button></router-link>
+        <router-link to='/setup'><md-button class="router-link-btn">new setup</md-button></router-link>
+        <router-link to='/about'><md-button class="router-link-btn">about</md-button></router-link>
+      </div>
+    </md-toolbar>
+      <img class="logo-home" src="../assets/logo-fond-blanc.png">
+      <h1>Control and supervise your personal data on the web</h1>
+      <md-button href="#" class="md-raised md-primary" id="access-btn">Connect to your data</md-button>
+      <div class="register">
+        <p class="not-register">New here ? Get started now</p>
+        <router-link to='/setup'><md-button class="md-icon-button md-raised md-primary" id="register-btn">
+          <md-icon>account_circle</md-icon>
+        </md-button></router-link>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo-navbar {
+  height: 60px;
+  margin-bottom: 8px;
+}
+.logo-home {
+  margin-top: 10%;
+}
+.navbar-btn {
+    position: absolute;
+    right: 0px;
+}
+
+.router-link-btn {
+  color: white;
+  text-decoration: none;
+}
+
+#access-btn {
+  margin-top: 40px;
+  margin-bottom: 15px;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+  font-weight: bold;
+}
+.register {
+  margin-top: 10px;
+}
+.register-btn {
+  float: right;
+}
+.not-register {
+  display: inline-block;
+}
+</style>
--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -21,8 +21,7 @@
           <md-input-container :class="{'md-input-invalid': errors.has('firstname')}" md-clearable>
             <md-icon>account_box</md-icon>
             <label>First name</label>
-              <md-input :class="{ 'has-error': errors.has('firstname'), 'form-group': true }" placeholder="Enter your first name" type="text" v-validate data-vv-rules="required|alpha" v-model="form.firstname" class="form-control" name="firstname" required autofocus></md-input>
-            </div>
+              <md-input :class="{ 'has-error': errors.has('firstname'), 'form-group': true }" placeholder="Enter your first name" type="text" v-validate data-vv-rules="required|alpha" v-model="firstname" class="form-control" name="firstname" ref="firstname_input" required autofocus></md-input>
           </md-input-container>
           <span v-if="errors.has('firstname')" class="help-block" id="error-msg">
             <md-icon class="md-warn">warning</md-icon>
@@ -32,8 +31,7 @@
           <md-input-container :class="{'md-input-invalid': errors.has('lastname')}" md-clearable>
             <md-icon>account_box</md-icon>
             <label>Last name</label>
-              <md-input :class="{ 'has-error': errors.has('lastname'), 'form-group': true }" placeholder="Enter your last name" type="text" v-validate data-vv-rules="required|alpha" v-model="form.lastname" class="form-control" name="lastname" required autofocus></md-input>
-            </div>
+              <md-input :class="{ 'has-error': errors.has('lastname'), 'form-group': true }" placeholder="Enter your last name" type="text" v-validate data-vv-rules="required|alpha" v-model="lastname" class="form-control" name="lastname" required autofocus></md-input>
           </md-input-container>
           <span v-if="errors.has('lastname')" class="help-block" id="error-msg">
             <md-icon class="md-warn">warning</md-icon>
@@ -43,7 +41,7 @@
           <md-input-container :class="{'md-input-invalid': errors.has('image')}">
             <md-icon>face</md-icon>
             <label>Avatar</label>
-            <md-file :class="{ 'has-error': errors.has('avatar'), 'form-group': true }" placeholder="Upload a picture to create an avatar" v-validate data-vv-rules="'ext:[jpg,png,bmp,svg]'" v-model="form.avatar" class="form-control" name="avatar" type="file" autofocus accept="image/*"></md-file>
+            <md-file :class="{ 'has-error': errors.has('avatar'), 'form-group': true }" placeholder="Upload a picture to create an avatar" v-validate data-vv-rules="image" v-model="avatar" class="form-control" name="avatar" type="file" autofocus accept="image/*"></md-file>
           </md-input-container>
           <span v-if="errors.has('avatar')" class="help-block" id="error-msg">
             <md-icon class="md-warn">warning</md-icon>
@@ -53,8 +51,7 @@
           <md-input-container :class="{'md-input-invalid': errors.has('phone')}" md-clearable>
             <md-icon>phone</md-icon>
             <label>Phone</label>
-              <md-input :class="{ 'has-error': errors.has('phone'), 'form-group': true }" placeholder="Enter your phone number" type="numeric" v-validate="{ required: true, regex: /^\d+(\.\d+)*$/ }" v-model="form.phone" class="form-control" name="phone" required autofocus></md-input>
-            </div>
+              <md-input :class="{ 'has-error': errors.has('phone'), 'form-group': true }" placeholder="Enter your phone number" type="numeric" v-validate="{ required: true, regex: /^\d+(\.\d+)*$/ }" v-model="phone" class="form-control" name="phone" required autofocus></md-input>
           </md-input-container>
           <span v-if="errors.has('phone')" class="help-block" id="error-msg">
             <md-icon class="md-warn">warning</md-icon>
@@ -64,8 +61,7 @@
           <md-input-container :class="{'md-input-invalid': errors.has('email')}" md-clearable>
             <md-icon>email</md-icon>
             <label>Email</label>
-              <md-input :class="{ 'has-error': errors.has('email'), 'form-group': true }" placeholder="Enter your email address" type="email" v-validate data-vv-rules="required|email" v-model="form.email" class="form-control" name="email" required autofocus></md-input>
-            </div>
+              <md-input :class="{ 'has-error': errors.has('email'), 'form-group': true }" placeholder="Enter your email address" type="email" v-validate data-vv-rules="required|email" v-model="email" class="form-control" name="email" required autofocus></md-input>
           </md-input-container>
           <span v-if="errors.has('email')" class="help-block" id="error-msg">
             <md-icon class="md-warn">warning</md-icon>
@@ -73,32 +69,38 @@
           </span>
         </form>
         <div class="new-id-btn">
-          <md-button href="#" class="md-button md-raised md-primary">
+          <md-button type="submit" v-on:click="retrieveFormData" class="md-button md-raised md-primary">
             Next
             <md-icon>send</md-icon>
           </md-button>
         </div>
       </md-card-content>
     </md-card>
-
   </main>
 </template>
 
 <script>
-export default {
-  name: 'Setup',
-  data() {
-    return {
-      form: {
-        firstname: null,
-        lastname: null,
+  export default {
+    name: 'Setup',
+    data() {
+      return {
+        firstname: '',
+        lastname: '',
         avatar: null,
-        email: null,
-        phone: null,
+        phone: '',
+        email: '',
+      };
+    },
+    methods: {
+      retrieveFormData() {
+        console.log('First name: ', this.firstname); // eslint-disable-line no-console
+        console.log('Last name: ', this.lastname); // eslint-disable-line no-console
+        console.log('Avatar file path: ', this.avatar); // eslint-disable-line no-console
+        console.log('Phone: ', this.phone); // eslint-disable-line no-console
+        console.log('Email: ', this.email); // eslint-disable-line no-console
       },
-    };
-  },
-};
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
